4424b3b919aff55b5a9ed88148928a8fe6f9c909
mikeyd <mdeguzis@gmail.com>
2015-08-13 18:31:34 -0400

    Update RL-fix-gamepad.sh

--- game-fixes/rocket-league/RL-fix-gamepad.sh
+++ game-fixes/rocket-league/RL-fix-gamepad.sh
@@ -2,8 +2,22 @@
 
 echo -e "Patching Rocket Leage (Steam) for Windows\n"
 
-cp -v x360ce.ini "/home/desktop/.wine/drive_c/Program Files/Steam/steamapps/common/rocketleague/Binaries/Win32/"
-cp -v xinput1_3.dll "/home/dekstop/.wine/drive_c/Program Files/Steam/steamapps/common/rocketleague/Binaries/Win32/"
+if [[ -d "home/desktop/.PlayOnLinux" ]]; then
+
+  cp -v x360ce.ini "/home/desktop/.PlayOnLinux/drive_c/Program Files/Steam/steamapps/common/rocketleague/Binaries/Win32/"
+  cp -v xinput1_3.dll "/home/desktop/.PlayOnLinux/drive_c/Program Files/Steam/steamapps/common/rocketleague/Binaries/Win32/"
+
+elif [[ -d "home/desktop/.cxoffice" ]]; then
+
+  cp -v x360ce.ini "/home/desktop/.cxoffice/drive_c/Program Files/Steam/steamapps/common/rocketleague/Binaries/Win32/"
+  cp -v xinput1_3.dll "/home/cxoffice/.PlayOnLinux/drive_c/Program Files/Steam/steamapps/common/rocketleague/Binaries/Win32/"
+
+elif [[ -d "home/desktop/.wine" ]]; then
+
+  cp -v x360ce.ini "/home/desktop/.wine/drive_c/Program Files/Steam/steamapps/common/rocketleague/Binaries/Win32/"
+  cp -v xinput1_3.dll "/home/desktop/.wine/drive_c/Program Files/Steam/steamapps/common/rocketleague/Binaries/Win32/"
+
+fi
 
 echo -e "Please restart Steam for Windows under the dekstop user\n"
 echo -e "This is intended only for wired/wireless XB360 controllers"
