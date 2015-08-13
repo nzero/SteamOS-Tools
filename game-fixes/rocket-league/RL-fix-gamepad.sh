62b9ba44c41fca17701005a21410c816cfe9ea36
mikeyd <mdeguzis@gmail.com>
2015-08-13 18:28:50 -0400

    Update RL-fix-gamepad.sh

--- game-fixes/rocket-league/RL-fix-gamepad.sh
+++ game-fixes/rocket-league/RL-fix-gamepad.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-cp x360ce.ini "/home/desktop/.wine/drive_c/Program Files/Steam/steamapps/common/rocketleague/Binaries/Win32/"
-cp xinput1_3.dll "/home/dekstop/.wine/drive_c/Program Files/Steam/steamapps/common/rocketleague/Binaries/Win32/"
+echo -e "Patching Rocket Leage (Steam) for Windows\n"
 
-echo -e "Please restart Steam under the dekstop user\n"
+cp -v x360ce.ini "/home/desktop/.wine/drive_c/Program Files/Steam/steamapps/common/rocketleague/Binaries/Win32/"
+cp -v xinput1_3.dll "/home/dekstop/.wine/drive_c/Program Files/Steam/steamapps/common/rocketleague/Binaries/Win32/"
+
+echo -e "Please restart Steam for Windows under the dekstop user\n"
 echo -e "This is intended only for wired/wireless XB360 controllers"
 
