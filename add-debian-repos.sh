a98f2f14af0fb98b87895bdebe8d613093781613
mikeyd <mdeguzis@gmail.com>
2015-10-08 07:57:02 -0400

    update old alchemist repo script

--- add-debian-repos.sh
+++ add-debian-repos.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
-
 # -----------------------------------------------------------------------
 # Author: 	Michael DeGuzis
 # Git:		https://github.com/ProfessorKaos64/SteamOS-Tools
 # Scipt Name:	add-debian-repos.sh
-# Script Ver:	0.2.0
+# Script Ver:	0.2.9
 # Description:	This script automatically enables debian repositories
 #
 #		See: https://wiki.debian.org/AptPreferences#Pinning
 #
 # Usage:	./add-debian-repos [install|uninstall|--help]
+# Extra args:	--enable-testing
 # ------------------------------------------------------------------------
 
 # remove old custom files
 rm -f "log.txt"
+
+# Specify a final arg for any extra options to build in later
+# The command being echo'd will contain the last arg used.
+# See: http://www.cyberciti.biz/faq/linux-unix-bsd-apple-osx-bash-get-last-argument/
+export final_opts=$(echo "${@: -1}")
 
 # set default action if no args are specified
 install="yes"
@@ -25,25 +30,71 @@ elif [[ "$1" == "uninstall" ]]; then
     	install="no"
 fi
 
+# enable test repo if desired
+if [[ "$final_opts" == "--enable-testing" ]]; then
+	
+	test_repo="yes"
+	
+elif [[ "$final_opts" != "--enable-testing" ]]; then
+    	
+    	test_repo="no"
+    	# ensure testing repo is removed
+    	sudo sed -ei '/Libregeek Debian testing repository/,+1d' "/etc/apt/sources.list.d/steamos-tools.list"
+fi
+
+check_gpg()
+{
+	if [[ "$scriptdir" == "" ]]; then
+	
+		#  Find where the user cloned SteamOS-Tools to
+		echo -e "==INFO==\nadd-debian-repos.sh was run from a foreign directory!"
+		echo -e "Assessing location of SteamoS-Tools, please wait...\n"
+		scriptdir=$(find / -name "SteamOS-Tools" 2> /dev/null)
+		
+	fi
+
+	echo -e "==> Importing GPG keys\n"
+	sleep 1s
+
+	# Key Desc: Libregeek Signing Key
+	# Key ID: 34C589A7
+	# Full Key ID: 8106E72834C589A7
+	echo -ne "Adding Libregeek public signing key: " && \
+	$scriptdir/utilities/gpg-import.sh 8106E72834C589A7 2> /dev/null
+	
+}
+
 funct_set_vars()
 {
 	# Set default user options
 	reponame="wheezy"
 	backports_reponame="wheezy-backports"
+	multimedia_reponame="deb-multimedia"
+	steamos_tools_reponame="steamos-tools"
 	
 	# tmp vars
 	sourcelist_tmp="${reponame}.list"
 	backports_sourcelist_tmp="${backports_reponame}.list"
+	multimedia_sourcelist_tmp="${multimedia_reponame}.list"
+	steamos_tools_sourcelist_tmp="${steamos_tools_reponame}.list"
+	
 	prefer_tmp="${reponame}"
 	backports_prefer_tmp="${backports_reponame}"
+	multimedia_prefer_tmp="${multimedia_reponame}"
 	steamos_prefer_tmp="steamos"
+	steamos_tools_prefer_tmp="${steamos_tools_reponame}"
 	
 	# target vars
 	sourcelist="/etc/apt/sources.list.d/${reponame}.list"
 	backports_sourcelist="/etc/apt/sources.list.d/${backports_reponame}.list"
+	multimedia_sourcelist="/etc/apt/sources.list.d/${multimedia_reponame}.list"
+	steamos_tools_sourcelist="/etc/apt/sources.list.d/${steamos_tools_reponame}.list"
+	
 	prefer="/etc/apt/preferences.d/${reponame}"
 	backports_prefer="/etc/apt/preferences.d/${backports_reponame}"
+	multimedia_prefer="/etc/apt/preferences.d/${multimedia_reponame}"
 	steamos_prefer="/etc/apt/preferences.d/steamos"
+	steamos_tools_prefer="/etc/apt/preferences.d/${steamos_tools_reponame}"
 }
 
 show_help()
@@ -79,7 +130,23 @@ main()
 	
 	if [[ "$install" == "yes" ]]; then
 		clear
-		echo -e "==> Adding Debian ${reponame} and ${backports_reponame} repositories\n"
+		
+		# Check/add gpg key for libregeek
+		check_gpg
+		
+		cat <<-EOF
+
+		==> Adding Debian ${reponame}, ${backports_reponame}, and ${steamos_tools_reponame} 
+		EOF
+		
+		if [[ "$test_repo" == "yes" ]]; then
+		
+			echo -ne "    [SteamOS-Tools testing enabled]\n"
+			
+		fi
+		
+		echo ""
+		
 		sleep 1s
 		
 		# Check for existance of /etc/apt/preferences file (deprecated, see below)
@@ -113,6 +180,15 @@ main()
 			sudo mv ${backports_prefer} ${backports_prefer}.bak
 			sleep 1s
 		fi
+		
+		# Check for existance of /etc/apt/preferences.d/{steamos_tools_prefer} file
+		if [[ -f ${steamos_tools_prefer} ]]; then
+			# backup preferences file
+			echo -e "==> Backing up ${steamos_tools_prefer} to ${steamos_tools_prefer}.bak\n"
+			sudo mv ${steamos_tools_prefer} ${steamos_tools_prefer}.bak
+			sleep 1s
+		fi
+		
 	
 		# Create and add required text to preferences file
 		# Verified policy with apt-cache policy
@@ -129,7 +205,7 @@ main()
 		cat <<-EOF > ${backports_prefer_tmp}
 		Package: *
 		Pin: origin ""
-		Pin-Priority:100
+		Pin-Priority:120
 		
 		Package: *
 		Pin: release o=Debian 
@@ -146,10 +222,34 @@ main()
 		Pin-Priority: 900
 		EOF
 		
+		# modify pinning based on setup for testing or not
+		if [[ "$test_repo" == "no" ]]; then
+			
+			cat <<-EOF > ${steamos_tools_prefer_tmp}
+			Package: *
+			Pin: release n=alchemist
+			Pin-Priority:150
+			EOF
+		
+		elif [[ "$test_repo" == "yes" ]]; then
+		
+			cat <<-EOF > ${steamos_tools_prefer_tmp}
+			Package: *
+			Pin: release n=alchemist
+			Pin-Priority:150
+			
+			Package: *
+			Pin: release n=alchemist_testing
+			Pin-Priority:150
+			EOF
+			
+		fi
+		
 		# move tmp var files into target locations
 		sudo mv  ${prefer_tmp}  ${prefer}
 		sudo mv  ${backports_prefer_tmp}  ${backports_prefer}
 		sudo mv  ${steamos_prefer_tmp}  ${steamos_prefer}
+		sudo mv  ${steamos_tools_prefer_tmp}  ${steamos_tools_prefer}
 		
 		#####################################################
 		# Check for lists in repos.d
@@ -170,7 +270,14 @@ main()
 	        	sudo mv ${backports_sourcelist} ${backports_sourcelist}.bak
 	        	sleep 1s
 		fi
-	
+		
+		if [[ -f ${steamos_tools_sourcelist} ]]; then
+	        	# backup sources list file
+	        	echo -e "==> Backing up ${steamos_tools_sourcelist} to ${steamos_tools_sourcelist}.bak\n"
+	        	sudo mv ${steamos_tools_sourcelist} ${steamos_tools_sourcelist}.bak
+	        	sleep 1s
+		fi
+
 		#####################################################
 		# Create and add required text to wheezy.list
 		#####################################################
@@ -186,46 +293,87 @@ main()
 		cat <<-EOF > ${backports_sourcelist_tmp}
 		deb http://http.debian.net/debian wheezy-backports main
 		EOF
+		
+		# packages.libregeek.org
+		if [[ "$test_repo" == "no" ]];then
+		
+			cat <<-EOF > ${steamos_tools_sourcelist_tmp}
+			# Libregeek Debian repository
+			deb http://packages.libregeek.org/SteamOS-Tools/ alchemist main games
+			deb-src http://packages.libregeek.org/SteamOS-Tools/ alchemist main games
+			EOF
+			
+		elif [[ "$test_repo" == "yes" ]];then
+		
+			cat <<-EOF > ${steamos_tools_sourcelist_tmp}
+			# Libregeek Debian repository
+			deb http://packages.libregeek.org/SteamOS-Tools/ alchemist main games
+			deb-src http://packages.libregeek.org/SteamOS-Tools/ alchemist main games
+			
+			# Libregeek Debian testing repository
+			deb http://packages.libregeek.org/SteamOS-Tools/ alchemist_testing main
+			EOF
+			
+		fi
 
 		# move tmp var files into target locations
 		sudo mv  ${sourcelist_tmp} ${sourcelist}
 		sudo mv  ${backports_sourcelist_tmp} ${backports_sourcelist}
-
+		sudo mv  ${steamos_tools_sourcelist_tmp} ${steamos_tools_sourcelist}
+		
 		# Update system
 		echo -e "\n==> Updating index of packages...\n"
 		sleep 2s
 		sudo apt-get update
-	
+
 		#####################################################
 		# Remind user how to install
 		#####################################################
+		
 		clear
-		echo -e "\n###########################################################"
-		echo "How to use"
-		echo -e "###########################################################"
-		echo -e "\nYou can now not only install package from the SteamOS repository," 
-		echo -e "but also from the Debian repository with either:\n\n"
-		echo -e "'sudo apt-get install <package_name>'"
-		echo -e "'sudo apt-get -t [wheezy|wheezy-backports] install <package_name>'\n"
-		echo -e "Warning: If the apt package manager seems to want to remove a lot"
-		echo -e "of packages you have already installed, be very careful about proceeding.\n"
+		cat <<-EOF
+		#################################################################
+		How to use
+		#################################################################
+		You can now not only install package from the SteamOS repository,
+		but also from the Debian and Libregeek repositories with either:
+		
+		'sudo apt-get install <package_name>'
+		'sudo apt-get -t [release] install <package_name>'
+		
+		Warning: If the apt package manager seems to want to remove a lot
+		of packages you have already installed, be very careful about proceeding.
+		
+		EOF
 	
 	elif [[ "$install" == "no" ]]; then
 		clear
-		echo -e "\n==> Removing debian repositories...\n"
+		echo -e "==> Removing debian repositories...\n"
 		sleep 2s
 		
-		# original files
+		# sourcelists (original)
 		sudo rm -f ${sourcelist}
 		sudo rm -f ${backports_sourcelist}
+		sudo rm -f ${multimedia_sourcelist}
+		sudo rm -f ${steamos_tools_sourcelist}
+		
+		# preference files (original)
 		sudo rm -f ${prefer}
 		sudo rm -f ${steamosprefer}
+		sudo rm -f ${mulitmedia_prefer}
+		sudo rm -f ${steamos_tools_prefer}
 		
-		# backups
+		# sourcelist backups
 		sudo rm -f ${sourcelist}.bak
 		sudo rm -f ${backports_sourcelist}.bak
+		sudo rm -f ${multimedia_sourcelist}.bak
+		sudo rm -f ${steamos_tools_sourcelist}.bak
+		
+		# prefer backups
 		sudo rm -f ${prefer}.bak
 		sudo rm -f ${steamosprefer}.bak
+		sudo rm -f ${mulitmedia_prefer}.bak
+		sudo rm -f ${steamos_tools_prefer}.bak
 		
 		sleep 2s
 		sudo apt-get update
